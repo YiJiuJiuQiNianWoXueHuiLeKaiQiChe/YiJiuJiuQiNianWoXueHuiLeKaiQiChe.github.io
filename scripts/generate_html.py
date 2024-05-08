1c10fc4545321e9655d7a194be0c805140c21f59
weyung <1653254230@qq.com>
2024-05-08 15:16:59 +0800

    update

--- scripts/generate_html.py
+++ scripts/generate_html.py
@@ -10,7 +10,7 @@ def generate_html(members):
     env = Environment(loader=FileSystemLoader('templates'))
     template = env.get_template('members_template.html')
     html_output = template.render(members=members)
-    with open('output/members.html', 'w+') as file:
+    with open('dist/members.html', 'w+') as file:
         file.write(html_output)
 
 if __name__ == "__main__":
